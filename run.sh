f8badd5419de3c3357bd4eab85578540e2841d77
qlearwater <amy.qing.ying@gmail.com>
2026-05-12 18:36:20 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 set -e
+set -x
+
+export PYTHONUNBUFFERED=1
 
 echo "STARTING DPS JOB"
 
-python hls_landsat_pipeline_dps.py \
+basedir=$(dirname "$(readlink -f "$0")")
+
+python "${basedir}/hls_landsat_pipeline_dps.py" \
     --mgrs_tile $1 \
     --date $2
 
